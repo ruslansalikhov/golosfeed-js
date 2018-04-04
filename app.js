c63b09d588eb9d05d6ff0119345bce3d85d14fdd
Georg Sawtschuk <georg.the.moon@googlemail.com>
2018-04-04 13:31:20 +0200

    adopted to golos-js

--- app.js
+++ app.js
@@ -14,59 +14,14 @@ if(!('node' in config)) { config['node'] = 'wss://ws.golos.io'; }
 if(!('peg' in config)) { config['peg'] = false; }
 if(!('peg_multi' in config)) { config['peg_multi'] = 1; }
 
-var options = {url: config['node']}
-var { TransactionBuilder, Login } = require('golosjs-lib');
-var {Client} = require('steem-rpc');
-var Api = Client.get(options, true);
-var request = require('request');
-require('./lib/helpers');
-// Needs to be global scope to access elsewhere
-var user = new Login();
+const golos = require("golos-js");
+
+golos.config.set("websocket", config.node);
 
 var shouldPublish = process.argv.length > 2 && process.argv[2] == "publishnow";
 var testrate = process.argv.length > 2 && process.argv[2] == "test";
 
 console.log('...');
-
-function loginAccount(username, wif, roles, callback) {
-    user.setRoles(roles);
-
-    Api.initPromise.then(function(r) {
-        // step 1. find the account
-        Api.database_api().exec("get_accounts", [[username]]).then(function(res) {
-            log('finding account', username);
-            if(res.length < 1) {
-                console.error('account not found')
-                return callback(true, null);
-            }
-            var account = res[0];
-            // load the keys from the account
-            var user_data = {
-                accountName: username,
-                auths: {
-                    owner: account.owner.key_auths,
-                    active: account.active.key_auths,
-                    posting: account.posting.key_auths
-                },
-                privateKey: config.wif
-            };
-            // try to log in
-            log('attempting to login account', username);
-            try {
-                var success_key = user.checkKeys(user_data);
-            } catch(e) {
-                success_key = false;
-                console.error('error logging in:', e);
-            }
-            if(success_key) {
-                log('logged in');
-                callback(false, user);
-            } else {
-                console.error('failed to log in');
-            }
-        });
-    });
-}
 
 var get_price = function(callback) {
     exchange.get_pair('golos','xau', function(price) {
@@ -75,14 +30,11 @@ var get_price = function(callback) {
     });
 }
 
-function publish_feed(rate, account_data) {
+async function publish_feed(rate) {
     try {
-        var tr = new TransactionBuilder();
         var ex_data = rate.toFixed(3) + " GBG";
-        if(testrate) {
-            log('Amount is: ', ex_data);
-            return;
-        }
+        log('Amount is: ', ex_data);
+
         var quote = 1;
         if(config.peg) {
             var pcnt = ((1 - config['peg_multi']) * 100).toFixed(2)
@@ -90,46 +42,28 @@ function publish_feed(rate, account_data) {
             log('Original price (pre-peg):', ex_data);
             quote = 1 / config['peg_multi'];
         }
-        var feed_data = {
-            publisher: account_data.name,
-            exchange_rate: {base: ex_data, quote: quote.toFixed(3) + " GOLOS"}
-        }
-        tr.add_type_operation("feed_publish", feed_data);
-        tr.process_transaction(account_data, null, true)
-    } catch(e) {
+        var feed_data = { base: ex_data, quote: quote.toFixed(3) + " GOLOS" };
+
+        await golos.broadcast.feedPublishAsync(config.wif, config.name, feed_data);
+
+    } catch (e) {
         console.error(e);
     }
     log('Data published at: ', ""+new Date())
     console.log();
 }
 
-function main(account_data) {
-    get_price(function(err,price) {
+async function main(account_data) {
+    get_price(async function(err,price) {
         if(err) {
-            return console.error('error loading prices, will retry later');
+            console.error('error loading prices, will retry later');
+            process.exit(1);
         }
         log('GOLOS/XAU(mg) is ', price.toFixed(3));
-        publish_feed(price, account_data);
+        await publish_feed(price);
+        process.exit(0);
     });
 }
 
-loginAccount(config.name, config.wif, ['active'], function(err,account_data) {
-    if(err) {
-        console.error('account failed to log in...', err);
-        return process.exit();
-    }
-    log('Successfully logged into user', account_data.name);
-    console.log();
-    if(shouldPublish || testrate) {
-        log('Publishing immediately, then every %s minute(s)',config.interval);
-        main(account_data);
-    } else {
-        log('Not publishing immediately');
-        log('If you want to update your price feed RIGHT NOW, use node app.js publishnow');
-    }
-    console.log();
-    // convert interval to minutes
-    var interval = parseInt(config.interval) * 1000 * 60;
-    setInterval(function() { main(account_data) }, interval)
-});
+main();
 
